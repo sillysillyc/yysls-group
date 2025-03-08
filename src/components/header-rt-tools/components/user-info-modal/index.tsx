5466ea46a562566bee501f60832e456fff5bf620
ChenJunff <270757002@qq.com>
2025-03-09 01:24:52 +0800

    feat: PersonalCenter 更新文本，退出登录操作优化， UserInfoModal 展示优化

--- src/components/header-rt-tools/components/user-info-modal/index.tsx
+++ src/components/header-rt-tools/components/user-info-modal/index.tsx
@@ -1,8 +1,10 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { Form, Modal, type ModalProps } from 'antd';
+import { AndroidOutlined, ManOutlined, WomanOutlined } from '@ant-design/icons';
 
 import { IUserInfo } from '@/helpers/services';
+import { genderMap } from '@/helpers/constants';
 
 interface IUserInfoModalProps extends ModalProps {
   userInfo: IUserInfo | null;
@@ -13,20 +15,31 @@ const { Item: FormItem } = Form;
 export const UserInfoModal = memo((props: IUserInfoModalProps) => {
   const { open, userInfo, ...restProps } = props;
 
+  const genderRenderer = useMemo(() => {
+    switch (userInfo?.gender) {
+      case genderMap.male:
+        return <ManOutlined />;
+      case genderMap.female:
+        return <WomanOutlined />;
+      default:
+        return <AndroidOutlined />;
+    }
+  }, [userInfo?.gender]);
+
   return (
-    <Modal centered width={800} open={open} {...restProps}>
+    <Modal title="个人信息" centered width={800} open={open} {...restProps}>
       <Form>
         <FormItem label="ID">
-          <div>{userInfo?.accountId}</div>
+          <div>{userInfo?.id}</div>
         </FormItem>
         <FormItem label="名称">
-          <div>{userInfo?.accountName}</div>
+          <div>{userInfo?.name}</div>
         </FormItem>
         <FormItem label="创建时间">
-          <div>{userInfo?.createdAt}</div>
+          <div>{userInfo?.createTime}</div>
         </FormItem>
         <FormItem label="性别">
-          <div>{userInfo?.accountGender}</div>
+          <div>{genderRenderer}</div>
         </FormItem>
       </Form>
     </Modal>
