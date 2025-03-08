e09a62d8cc1d423a0e1c98b09d648c817c7390ee
ChenJunff <270757002@qq.com>
2025-03-09 01:15:09 +0800

    feat: store 新增对 userInfo 持久化存储, 移除不需要的字段

--- src/stores/slices/user/slice.ts
+++ src/stores/slices/user/slice.ts
@@ -1,21 +1,21 @@
 import type { UserSliceInfo, UserState } from './types';
 
+export const loginTabKeys = {
+  login: 'login',
+  register: 'register',
+} as const;
+
 const initialUserState: UserState = {
-  username: null,
   userInfo: null,
   userInfoModalOpen: false,
+  loginTabKey: loginTabKeys.login,
 };
 
 export const createUserSlice = (set: any): UserSliceInfo => ({
   ...initialUserState,
-  login: (name) => {
-    // set({ username: name });
-  },
-  logout: () => {
-    set({ username: null });
-  },
   setUserInfo: ({ userInfo }) => {
     set({ userInfo });
   },
   setUserInfoModalOpen: ({ open }) => set({ userInfoModalOpen: open ?? false }),
+  setLoginTabKey: ({ key }) => set({ loginTabKey: key }),
 });
