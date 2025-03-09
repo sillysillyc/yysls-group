bb332642a02399fde74ff8a1f87e3b80333089c7
ChenJunff <270757002@qq.com>
2025-03-09 18:24:01 +0800

    feat: constants 新增角色创建、更新提示语常量

--- src/helpers/constants/messages.ts
+++ src/helpers/constants/messages.ts
@@ -4,5 +4,13 @@ export const messages = {
       success: '删除成功',
       fail: '删除失败',
     },
+    create: {
+      success: '创建成功',
+      fail: '创建失败',
+    },
+    update: {
+      success: '编辑成功',
+      fail: '编辑失败',
+    },
   },
 };
