e6d18a6852947b7edbfd4912eed5acbd9b61f90f
ChenJunff <270757002@qq.com>
2025-03-09 17:53:14 +0800

    feat:  store 新增 charactersOperModalOpen charactersOperInfo 用于角色新增、编辑

--- src/stores/slices/characters/slice.ts
+++ src/stores/slices/characters/slice.ts
@@ -1,9 +1,12 @@
 import { type Mutate, type StoreApi } from 'zustand';
 import type { CharactersSliceInfo, CharactersState } from './types';
 import { type ZustandMiddleWares } from '@/stores/types';
+import { fetchQueryCharacterList } from '@/helpers/services';
 
 const initialCharactersState: CharactersState = {
   charactersList: null,
+  charactersOperModalOpen: false,
+  charactersOperInfo: null,
 };
 
 export const createCharactersStateSlice = (
@@ -12,5 +15,17 @@ export const createCharactersStateSlice = (
   ...initialCharactersState,
   setCharactersList: ({ list }) => {
     set({ charactersList: [...list] });
+  },
+  queryCharactersList: async (params) => {
+    try {
+      const result = await fetchQueryCharacterList(params);
+      set({ charactersList: [...(result.data.characters ?? [])] });
+      return Promise.resolve(result.data);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  },
+  setCharactersOperInfo: (payload: CharactersState['charactersOperInfo']) => {
+    set({ charactersOperInfo: payload ? { ...payload } : payload });
   },
 });
--- src/stores/slices/characters/types/state.d.ts
+++ src/stores/slices/characters/types/state.d.ts
@@ -1,5 +1,10 @@
-import { type IFetchQueryCharacterListData } from '@/helpers/services';
+import { type ICharacterInfo } from '@/helpers/services';
 
 export type CharactersState = {
-  charactersList: IFetchQueryCharacterListData[] | null;
+  charactersList: ICharacterInfo[] | null;
+  /**
+   * 新增 / 编辑 角色信息弹框是否打开
+   */
+  charactersOperModalOpen: boolean;
+  charactersOperInfo: ICharacterInfo | null;
 };
