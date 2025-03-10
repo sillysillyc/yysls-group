be8ba9705121c543a9d40aaf9311d4f4014a75b1
陈俊帆 <chenjunfan1@cmiot.chinamobile.com>
2025-03-10 17:31:44 +0800

    feat: 调整 store 目录结构

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,1 +1,41 @@
-export * from './useAppStore';
+import { create } from 'zustand';
+import { persist, createJSONStorage, devtools } from 'zustand/middleware';
+
+import {
+  createUserSlice,
+  createGlobalSlice,
+  createCharactersStateSlice,
+  type GlobalSliceInfo,
+  type UserSliceInfo,
+  type CharactersSliceInfo,
+} from './slices';
+import { ZustandMiddleWares } from './types';
+
+export type StoreState = GlobalSliceInfo & UserSliceInfo & CharactersSliceInfo;
+
+export * from './slices';
+
+export const useAppStore = create<StoreState, ZustandMiddleWares>(
+  devtools(
+    persist(
+      (set, get) => ({
+        ...createGlobalSlice(set), // 组合全局分片
+        ...createUserSlice(set), // 组合用户分片
+        ...createCharactersStateSlice(set), // 组合用户分片
+        getState: get,
+      }),
+      {
+        name: 'yysls-storage', // 存储的键名
+        storage: createJSONStorage(() => localStorage), // 使用 localStorage 作为存储
+        partialize: (state) => ({
+          // 只持久化需要的状态
+          userInfo: state.userInfo,
+        }),
+      }
+    )
+  )
+);
+
+useAppStore.subscribe((state) => {
+  console.log('[Zustabd]: ', state);
+});
